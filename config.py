f446fe99a1fedbc09f9d9b7bb3ca31b8b3dc8600
feeri-irwn <erif45810@gmail.com>
2024-11-13 02:03:18 +0700

    UTS

--- config.py
+++ config.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://UTSFeriIrawan0619_moonrecent:be64c1c79667ab59242762e97125b322d0f4ae3d@pys1s.h.filess.io:3306/UTSFeriIrawan0619_moonrecent'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    'pool_size': 3,
+    'max_overflow': 0
+}
+
 db = SQLAlchemy(app)
 
 app.app_context().push()
